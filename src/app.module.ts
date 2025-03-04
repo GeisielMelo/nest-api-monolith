3fce6d948448d7a608c729cb6aa00196494aafdc
Geisiel <geisiel.nascimento@gmail.com>
2025-03-04 01:01:34 -0300

    feat: mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,19 @@ import { UserModule } from './modules/user/user.module';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: () => ({
-        type: 'sqlite',
-        database: join(__dirname, 'database', 'db.sqlite'),
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
